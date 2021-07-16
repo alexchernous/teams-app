75584c77e6ff6ccbc448a38fafe05a4ca13edb8c
unknown <alex.a.chernous@gmail.com>
2021-07-16 16:13:02 -0400

    removing endpoints

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 const constants = {
   endpoints: {
-    users: 'https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/users/',
-    user: (userId) => `https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/users/${userId}`,
-    teams: 'https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/teams/',
-    team: (teamId) => `https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/teams/${teamId}`,
+    users: '/',
+    user: (userId) => `/${userId}`,
+    teams: '/',
+    team: (teamId) => `/${teamId}`,
   },
   tableHeaders: [
     'Avatar',
